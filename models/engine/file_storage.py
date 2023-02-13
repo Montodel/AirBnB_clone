9d0b3c369a92590e0090b8dcd659f2d5aa09cdd8
Montodel <suleimanmoha120@gmail.com>
2023-02-13 13:08:17 +0000

    fixing error in models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,55 +1,66 @@
 #!/usr/bin/python3
-'''
-    Define class FileStorage
-'''
-import json
-import models
+"""file_storage module containing file_storage class """
+import os
 
 
 class FileStorage:
-    '''
-        Serializes instances to JSON file and deserializes to JSON file.
-    '''
-    __file_path = "file.json"
+    """
+    FileStorage class: contains methods to serialize objects to JSON
+    and to deserialize JSON files to objects
+    """
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
-        '''
-            Return the dictionary
-        '''
-        return self.__objects
+        """
+        Returns __objects dictionary
+        """
+        return type(self).__objects
 
     def new(self, obj):
-        '''
-            Set in __objects the obj with key <obj class name>.id
-            Aguments:
-                obj : An instance object.
-        '''
-        key = str(obj.__class__.__name__) + "." + str(obj.id)
-        value_dict = obj
-        FileStorage.__objects[key] = value_dict
+        """
+        Sets obj value in __objects dict with key <obj class name>.id
+        """
+        objects_value = obj
+        objects_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        type(self).__objects[objects_key] = objects_value
 
     def save(self):
-        '''
-            Serializes __objects attribute to JSON file.
-        '''
-        objects_dict = {}
-        for key, val in FileStorage.__objects.items():
-            objects_dict[key] = val.to_dict()
+        """
+        Serializes __objects dict to JSON.
+        Writes File to __file_path.
+        """
+        from json import dump as jdu
+        from models.base_model import BaseModel
 
-        with open(FileStorage.__file_path, mode='w', encoding="UTF8") as fd:
-            json.dump(objects_dict, fd)
+        dict_to_serial = type(self).__objects.copy()
+        with open(type(self).__file_path, "w+") as File:
+            for key, value in dict_to_serial.items():
+                value_as_dict = value.to_dict()
+                dict_to_serial[key] = value_as_dict
+            jdu(dict_to_serial, File)
 
     def reload(self):
-        '''
-            Deserializes the JSON file to __objects.
-        '''
-        try:
-            with open(FileStorage.__file_path, encoding="UTF8") as fd:
-                FileStorage.__objects = json.load(fd)
-            for key, val in FileStorage.__objects.items():
-                class_name = val["__class__"]
-                class_name = models.classes[class_name]
-                FileStorage.__objects[key] = class_name(**val)
-        except FileNotFoundError:
-            pass
+        """
+        If the JSON file exists, deserializes it.
+        Opens file in read-only mode.
+        Converts the string reps back to objects.
+        """
+        from json import load as jlo
+        from models.base_model import BaseModel
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+        from models.user import User
+
+        new_dict = {}
+        cl = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+              "Place": Place, "Review": Review, "State": State, "User": User}
+        if os.path.isfile(type(self).__file_path) is True:
+            with open(type(self).__file_path, 'r') as File:
+                nd = jlo(File)
+            for key in nd:
+                type(self).__objects[key] = cl[nd[key]["__class__"]](**nd[key])
+
